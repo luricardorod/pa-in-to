d9136c8b15247c023132ec0ce198e907db58a84c
Rooklah <cj.070997@gmail.com>
2016-11-16 13:53:06 -0600

    Classe dibujable

---
+++ painto/painto/CDibujable.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CDibujable.h"
+
+
+CDibujable::CDibujable()
+{
+}
+
+
+CDibujable::~CDibujable()
+{
+}
---
+++ painto/painto/CDibujable.h
@@ -0,0 +1,8 @@
+#pragma once
+class CDibujable
+{
+public:
+	CDibujable();
+	~CDibujable();
+};
+
