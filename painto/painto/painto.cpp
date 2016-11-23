f0b2ec8615ff5ce422f7243ba621a18647af9c0b
F_A_R <F_A_R@DESKTOP-O7NVA5L>
2016-11-22 20:57:02 -0600

    SFML Ya funciona

--- painto/painto/painto.cpp
+++ painto/painto/painto.cpp
@@ -1,20 +1,21 @@
 #include "stdafx.h"
 #include <SFML\Graphics.hpp>
 #include "cDocumento.h"
+using namespace sf;
 
 int main()
 {
 	cDocumento canvas1;
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	RenderWindow window(VideoMode(200, 200), "SFML works!");
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Green);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
 		}
 
